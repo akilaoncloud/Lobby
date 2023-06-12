079ce8118bc030226ebed5e3a5acfcb20cdec1b7
Thiago Alexandre <70294974+akilaoncloud@users.noreply.github.com>
2023-06-12 01:41:00 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Bom dia, meu amor. Que seu dia seja abençoado! (esqueletofazendopassinhodecapoeira.gif)</h1>
+    return <h1>Bom dia, meu amorzinho. Que seu dia seja abençoado! (esqueletofazendopassinhodecapoeira.gif)</h1>
 }
 
 export default Home;
